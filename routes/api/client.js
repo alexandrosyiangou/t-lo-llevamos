182a7aec3fb9936e3c7325ede43e2ff9966721da
Alexandros Yiangou <alexyiangousolutions@gmail.com>
2020-03-13 22:46:14 +0200

    Define client and messenger routers

--- routes/api/client.js
+++ routes/api/client.js
@@ -1,7 +1,25 @@
 const ClientRouter = require('express').Router()
+const {insertClient} = require('../../controllers/client')
 
 ClientRouter.post('/insert', (req, res) => {
+  const {body} = req;
+  console.log(body)
+  // const response = await insertClient();
+  res.send('Insert Client Successful') 
+})
 
+ClientRouter.patch('/update', (req, res) => {
+  const {body} = req;
+  console.log(body)
+  // const response = await updateClient();
+  res.send('Update Client Successful') 
+})
+
+ClientRouter.delete('/delete', (req, res) => {
+  const {body} = req;
+  console.log(body)
+  // const response = await deleteClient();
+  res.send('Delete Client Successful') 
 })
 
 ClientRouter.get('/', (req, res) => res.send('Client Router'))
--- routes/api/messenger.js
+++ routes/api/messenger.js
@@ -1,7 +1,24 @@
 const MessengerRouter = require('express').Router()
 
 MessengerRouter.post('/insert', (req, res) => {
+  const {body} = req;
+  console.log(body)
+  // const response = await insertMessenger();
+  res.send('Insert Messenger Successful') 
+})
 
+MessengerRouter.patch('/update', (req, res) => {
+  const {body} = req;
+  console.log(body)
+  // const response = await updateMessenger();
+  res.send('Update Messenger Successful') 
+})
+
+MessengerRouter.delete('/delete', (req, res) => {
+  const {body} = req;
+  console.log(body)
+  // const response = await deleteMessenger();
+  res.send('Delete Messenger Successful') 
 })
 
 MessengerRouter.get('/', (req, res) => res.send('Messenger Router'))
