c7307f6ddc9cf332722b2418a84fc857c5ee5b01
Alexandros Yiangou <alexyiangousolutions@gmail.com>
2020-03-14 00:49:54 +0200

    Fix migrations

---
+++ knex/migrations/20200313231108_create_clients_table.js
@@ -0,0 +1,18 @@
+const tableName = 'clients'
+
+exports.up = async function(knex) {
+  await knex.schema.createTable(tableName, async (table) => {
+    table.increments('id').primary()
+    table.string('name', 30).notNullable().unique()
+    table.string('email', 30).notNullable().unique()
+    table.string('address', 50).notNullable().unique()
+    table.string('phoneNumber', 20).notNullable().unique()
+    table.string('directionMap', 100).notNullable().unique()
+    table.string('valueOfServices', 10).notNullable().unique()
+    table.string('assignedMessages', 100).notNullable().unique()
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTable(tableName)
+};
