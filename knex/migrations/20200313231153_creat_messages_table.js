d6836fd9ae34e907df8083b30b1c656717aa7df3
elena <elena-kii15@hotmail.com>
2020-03-13 23:38:08 +0200

    create_client_table

---
+++ knex/migrations/20200313231153_creat_messages_table.js
@@ -0,0 +1,18 @@
+const tableName = "Messages"
+exports.up = async function(knex) {
+    await knex.schema.createTable(tableName, async (table) => {
+        table.increments('id').primary()
+        table.string('name',30).notNullable().unique()
+        table.string('address',50).notNullable().unique()
+        table.string('closeEndDay',10).notNullable().unique()
+        table.string('phoneNumber',20).notNullable().unique()
+        table.string('courierRating',10).notNullable().unique()
+        table.string('dailyMoneyHandled',10).notNullable().unique()
+        table.string('earningsPercentage',10).notNullable().unique()
+        table.string('identificationCard',30).notNullable().unique()
+    })
+};
+
+exports.down = async function(knex) {
+    await knex.schema.dropTable(tableName)
+};
